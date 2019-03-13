7d8a29e52daeda7575ef8f092f97421edafc10c8
Jake Holmes <cakeeatsgirl@gmail.com>
2019-03-13 14:17:58 -0400

    Use dynamic value for hiding header name

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -59,11 +59,11 @@ class Header extends Component {
 
   // update hideName based on vertical scroll
   updateNameVisibility() {
-    if (window.scrollY < 230) {
+    if (window.scrollY < screen.height * 0.31) {
       if (this.state.hideName === false) {
         this.setState({ hideName: true });
       }
-    } else if (this.state.hideName === true){
+    } else if (this.state.hideName === true) {
       this.setState({ hideName: false });
     }
   }
