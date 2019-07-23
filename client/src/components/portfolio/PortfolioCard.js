845d129148b12d320dbd1193f2c1de183cf611c6
Jake Holmes <cakeeatsgirl@gmail.com>
2019-07-23 15:34:13 -0400

    Use carousel component & rename 'imageURLs' to 'images'

--- client/src/components/portfolio/PortfolioCard.js
+++ client/src/components/portfolio/PortfolioCard.js
@@ -10,11 +10,11 @@ function PortfolioCard({
   builtWith,
   githubURL,
   learnMoreURL,
-  imageURLs
+  images
 }) {
   return (
     <div className={styles.card}>
-      <div className={styles.imageWrapper} />
+      <Carousel images={images}/>
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
       <div className={styles.builtWith}>{builtWith}</div>
