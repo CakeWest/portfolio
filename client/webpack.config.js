e12220a1ed6d7acec570d4abb4c7b417bd21fa61
Jake West <cakeeatsgirl@gmail.com>
2020-11-30 20:10:14 -0500

    Update webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,14 +1,30 @@
 const path = require("path");
 const webpack = require("webpack");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const devMode = process.env.NODE_ENV !== "production";
 
+const plugins = [
+  new CleanWebpackPlugin(),
+  new HtmlWebpackPlugin({
+    template: "./src/index.html",
+    filename: "index.html",
+  }),
+];
+if (!devMode) {
+  plugins.push(
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    })
+  );
+}
+
 module.exports = {
   entry: "./src/index.js",
-  mode: "development",
+  plugins,
   module: {
     rules: [
       {
@@ -16,28 +32,47 @@ module.exports = {
         exclude: /(node_modules)/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/env"]
-        }
+          presets: ["@babel/env"],
+        },
       },
       {
-        test: /\.s?[ac]ss$/,
+        test: /\.module\.s(a|c)ss$/,
         use: [
-          MiniCssExtractPlugin.loader,
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
             options: {
               modules: true,
-              localIdentName: "[name]_[local]--[hash:base64:5]"
-            }
+              sourceMap: devMode,
+            },
           },
-          "sass-loader"
-        ]
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: devMode,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: devMode,
+            },
+          },
+        ],
       },
       {
         test: /\.(png|svg|jpe*g|gif)$/,
-        use: ["file-loader"]
-      }
-    ]
+        use: ["file-loader"],
+      },
+    ],
   },
   resolve: {
     alias: {
@@ -47,28 +82,18 @@ module.exports = {
       home: path.resolve(__dirname, "src/components/home/"),
       portfolio: path.resolve(__dirname, "src/components/portfolio/"),
       stack: path.resolve(__dirname, "src/components/stack/"),
-      contact: path.resolve(__dirname, "src/components/contact/")
-    }
+      contact: path.resolve(__dirname, "src/components/contact/"),
+    },
   },
   output: {
     path: path.resolve(__dirname, "dist/"),
     publicPath: "/",
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
   devServer: {
     contentBase: path.join(__dirname, "dist/"),
     port: 3000,
     publicPath: "/",
-    historyApiFallback: true
+    historyApiFallback: true,
   },
-  plugins: [
-    new CleanWebpackPlugin("dist", {}),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "style.[hash].css"
-    })
-  ]
 };
